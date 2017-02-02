307d7f92b4b45a8dffaeb959a721aeeb1e0bfb2f
Garren Smith <garren.smith@gmail.com>
2017-02-02 09:45:54 +0200

    add back used variables

--- test/test-abstract-mapreduce/evalfunc.js
+++ test/test-abstract-mapreduce/evalfunc.js
@@ -1,5 +1,6 @@
 'use strict';
 
-module.exports = function (func) {
+// eslint-disable-next-line no-unused-vars
+module.exports = function (func, emit, sum, log, isArray, toJSON) {
   return eval("'use strict'; (" + func.replace(/;\s*$/, "") + ");");
 };
